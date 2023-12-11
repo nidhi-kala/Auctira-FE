5b56188f308b7b684dc37c86f82b95f4434bbb99
Nidhi Kala <nidhikala95@gmail.com>
2023-12-10 23:32:40 -0500

    App is functional

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="p-5 text-lg text-center bg-green-50">
-    Welcome to Auctria's technical interview. Feel free to remove this component and start coding!
+  <div>
+    <header class="bg-blue-500 p-4">
+      <nav class="container mx-auto flex items-center justify-between">
+        <router-link to="/" class="text-white text-2xl font-semibold">
+          Ticket App
+        </router-link>
+        <div class="space-x-4">
+          <router-link to="/admin" class="text-white">
+            Admin
+          </router-link>
+          <router-link to="/cart" class="text-white">
+            Cart
+          </router-link>
+        </div>
+      </nav>
+    </header>
+    <main class="container mx-auto p-4">
+      <router-view></router-view>
+    </main>
   </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
+import router from '../router'; 
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+const app = createApp(App);
+app.use(pinia);
+app.use(router); // Use the router
+app.mount('#app');
